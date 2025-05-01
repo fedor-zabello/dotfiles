cfd2f077e293fb22bdd0ea9b856a354fe0729eda
Fedor Zabello <fedor.zabello@gmail.com>
2025-05-01 17:14:01 +0800

    autocomplit

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,12 +1,12 @@
-return { 
-  "nvim-treesitter/nvim-treesitter", 
-  build = ":TSUpdate",
-  config = function()
-    local config = require('nvim-treesitter.configs')
-    config.setup({
-      ensure_installed = { "lua", "java", "python", "javascript" },
-      highlight = { enable = true },
-      indent = { enable = true }
-    })
-  end
+return {
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local config = require("nvim-treesitter.configs")
+		config.setup({
+			autoinstall = true,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
