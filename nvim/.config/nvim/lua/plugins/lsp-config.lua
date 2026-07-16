bb0dd61779d18cf92ef88dcddf1be1289ed865b5
zabellofi <zabellofi@alfastrah.ru>
2026-07-16 20:14:23 +0300

    jdtls

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -9,7 +9,10 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "ts_ls" },
+				ensure_installed = { "lua_ls", "ts_ls", "jdtls" },
+				automatic_enable = {
+					exclude = { "java_language_server" },
+				},
 			})
 		end,
 	},
@@ -19,7 +22,7 @@ return {
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 			vim.lsp.config("*", { capabilities = capabilities })
-			vim.lsp.enable({ "lua_ls", "ts_ls", "kotlin_language_server" })
+			vim.lsp.enable({ "lua_ls", "ts_ls", "kotlin_language_server", "jdtls" })
 
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
